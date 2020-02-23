09594a39f9926be94abc61d06323e0b241a0398f
andela-aonifade <anuoluwapo.onifade@andela.com>
2020-02-23 23:34:25 +0100

    adds elasticsearch indexing

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,12 @@ class Product < ApplicationRecord
   validates :title, presence: true
   validates :country, presence: true
   validates :price, presence: true
+
+  mappings dynamic: false do
+    indexes :title, type: :text
+    indexes :description, type: :text, analyzer: :english
+    indexes :country, type: :text, analyzer: :english
+    indexes :tags, type: :text, analyzer: :english
+    indexes :price, type: :float
+  end
 end
