7e25860321d6e5ec6b5100ebf02bb47c262749d2
andela-aonifade <anuoluwapo.onifade@andela.com>
2020-02-23 01:48:27 +0100

    configures route to let react handle frontend routing and handles only api backend routing

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () => (
+  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+    <div className="jumbotron jumbotron-fluid bg-transparent">
+      <div className="container secondary-color">
+        <h1 className="display-4">Product Search</h1>
+        <p className="lead">
+          Check out our amazing product that is specific to your taste
+        </p>
+        <hr className="my-4" />
+        <Link to="/products" className="btn btn-lg custom-button" role="button">
+          View Products
+        </Link>
+      </div>
+    </div>
+  </div>
+);
